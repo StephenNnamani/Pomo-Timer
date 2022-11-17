d8a98f9c56f72af3a3987c75b3debe90a381b584
Nnamani Stephen <sir.stephennnamani@gmail.com>
2022-11-17 13:39:08 +0100

    My Pomo Timer

---
+++ Stephen's Project/WorkTimer App/WorkTimer App/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace WorkTimer_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Worker Worker = new Worker();
+            Console.WriteLine("What is your name, new guest?");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Good Day" + name);
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("You are welcomed to the day's tasks");
+            Console.WriteLine("Hope you are feeling well today :)");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Pease, how many tasks do you have for today?");
+            int numberOfTask = Convert.ToInt32(Console.ReadLine());
+            string[] TaskList = Worker.numberOfWork(numberOfTask);
+            Console.WriteLine("how many MINUTES will it take to accomplish each Task");
+            int[] sumTime = new int[TaskList.Length];
+            foreach(string task in TaskList)
+            {
+                int i = 0;
+                Console.WriteLine("How many minutes are you assigning to this task?");
+                Console.WriteLine(task);
+                sumTime[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //Worker.WorkerTimer(duration);
+        }
+    }
+}
---
+++ Stephen's Project/WorkTimer App/WorkTimer App/WorkTimer_App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimer_App
+{
+    internal class Worker
+    {
+            public string WorkerTimer(int duration)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Thread.Sleep(duration);
+
+                TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}"
+                , ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            return elapsedTime;
+            }
+            public static string[] numberOfWork(int numberOfTask)
+            {
+                string?[] tasks = new string[numberOfTask];
+                int i = 0;
+                while (numberOfTask > 0)
+                {
+                    Console.WriteLine("What is your Task number" + i);
+                    tasks[i] = Console.ReadLine();
+                }
+
+                return tasks;
+            }
+        }
+    
+}
